212094fe3f4d978c355c2fee3f5b823df6e4deec
tosh1ra <dilovar6033@gmail.com>
2026-04-14 19:16:07 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
+from sklearn.datasets import fetch_california_housing
 
 st.title('Hello World!')
 
 st.markdown('### AI')
+@st.cashe_data
 
+data = fetch_california_housing()
